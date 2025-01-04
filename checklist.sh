3a112dd5065f6eaab656112cf0cb879aa32f3687
YankovskiyVS <vld.s.yan567@gmail.com>
2025-01-04 17:44:35 +0300

    Add files via upload
    Bash code to run the python script

---
+++ checklist.sh
@@ -0,0 +1,9 @@
+#!/opt/homebrew/bin/bash
+printf "Create your own To-do list\n"
+printf "To-do list app\t"
+printf "Version: 1.0.0\n"
+printf "Creator: YankovskiyVS\n"
+printf "For more follow the GitHub:\n"
+printf "https://github.com/YankovskiyVS/ToDoList.git\n"
+python to_do_list.py
+
